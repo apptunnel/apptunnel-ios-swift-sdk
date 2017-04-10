413209d048c9ab9827b556dd9d5330031bfceee2
ofk88 <oguzfurkankaytanci@gmail.com>
2017-04-11 01:31:37 +0200

    reorganized file structure

---
+++ AppTunnelSwift.framework/Headers/ATSettings.h
@@ -0,0 +1,17 @@
+//
+//  ATSettings.h
+//  AppTunnel
+//
+//  Created by Oguz furkan Kaytanci on 4/11/17.
+//  Copyright © 2017 AppTunnel. All rights reserved.
+//
+
+#ifndef ATSettings_h
+#define ATSettings_h
+
+typedef enum {
+    kAppTunnelEnvironmentSandbox,
+    kAppTunnelEnvironmentProduction
+} AppTunnelEnvironment;
+
+#endif /* ATSettings_h */
